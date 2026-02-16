e2a819d295c0ce8dea889bf9a2b8638f9629637f
LuchikiSveta <ToBeVoid@yandex.ru>
2026-02-16 22:56:41 +0300

    add: Add table

--- src/main/GUI/AdminWindow.java
+++ src/main/GUI/AdminWindow.java
@@ -9,8 +9,6 @@ import java.util.List;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.event.CellEditorListener;
-import javax.swing.event.TableModelListener;
 
 import API.AttributeTypeProperties;
 import API.interfaces.IDBAttributeTypeCollection;
@@ -20,21 +18,13 @@ import javax.swing.JTabbedPane;
 import javax.swing.JSplitPane;
 import javax.swing.JList;
 import javax.swing.JTable;
-import javax.swing.JTextField;
 import javax.swing.AbstractListModel;
-import javax.swing.DefaultCellEditor;
-import javax.swing.JComboBox;
-import javax.swing.ListSelectionModel;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableCellEditor;
-import javax.swing.table.TableColumn;
-import javax.swing.table.TableModel;
 import javax.swing.JScrollPane;
 import javax.swing.SpringLayout;
 import javax.swing.JButton;
-import javax.swing.ScrollPaneConstants;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class AdminWindow extends JFrame {
 
@@ -63,47 +53,9 @@ public class AdminWindow extends JFrame {
 		
 		table = new JTable();
 		
-		JList<Object> list = new JList<Object>();
-		list.addMouseListener(new MouseAdapter() {
-			public void mouseClicked(MouseEvent e) {
-				
-				System.out.println(list.getSelectedValue());
-				
-				table.setModel(new DefaultTableModel(
-					new Object[][] {
-						{"5", "1"},
-						{"6", "2"},
-						{"7", "3"},
-					},
-					new String[] {
-						"\u041F\u0430\u0440\u0430\u043C\u0435\u0442\u0440", "\u0417\u043D\u0430\u0447\u0435\u043D\u0438\u0435"
-					}
-				) {
-					boolean[] columnEditables = new boolean[] {
-					false, true
-				};
-					public boolean isCellEditable(int row, int column) {
-						return columnEditables[column];
-					}
-				});
-				
-			}
-		});
-		
-		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		
 		IDBAttributeTypeCollection coll = session.getAttributeTypeCollection();
 		
-		list.setModel(new AbstractListModel<Object>() {
-			List<AttributeTypeProperties> values = coll.select();
-			public int getSize() {
-				return values.size();
-			}
-			public Object getElementAt(int index) {
-				return values.get(index);
-			}
-		});
-		splitPane.setLeftComponent(list);
+		List<AttributeTypeProperties> attrs = coll.select();
 		
 		JPanel panel = new JPanel();
 		splitPane.setRightComponent(panel);
@@ -116,6 +68,7 @@ public class AdminWindow extends JFrame {
 		panel.add(scrollPane);
 		
 		JButton btnNewButton = new JButton("Отменить");
+		btnNewButton.setEnabled(false);
 		sl_panel.putConstraint(SpringLayout.SOUTH, scrollPane, -6, SpringLayout.NORTH, btnNewButton);
 		sl_panel.putConstraint(SpringLayout.EAST, scrollPane, 8, SpringLayout.EAST, btnNewButton);
 		
@@ -126,9 +79,55 @@ public class AdminWindow extends JFrame {
 		panel.add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("Применить");
+		btnNewButton_1.setEnabled(false);
 		sl_panel.putConstraint(SpringLayout.SOUTH, btnNewButton_1, 0, SpringLayout.SOUTH, btnNewButton);
 		sl_panel.putConstraint(SpringLayout.EAST, btnNewButton_1, -6, SpringLayout.WEST, btnNewButton);
 		panel.add(btnNewButton_1);
+		
+		JScrollPane scrollPane_1 = new JScrollPane();
+		splitPane.setLeftComponent(scrollPane_1);
+		
+		JList<AttributeTypeProperties> list = new JList<>();
+		list.addMouseListener(new MouseAdapter() {
+			
+			public void mouseClicked(MouseEvent e) {
+				
+				AttributeTypeProperties attr = list.getSelectedValue();
+				
+				table.setModel(new DefaultTableModel(
+					new Object[][] {
+						{"Идентификатор", attr.attributeTypeID},
+						{"Наименование", attr.attributeName},
+						{"Тип значения", attr.valueAttributeType},
+					},
+					new String[] {
+						"\u041F\u0430\u0440\u0430\u043C\u0435\u0442\u0440", "\u0417\u043D\u0430\u0447\u0435\u043D\u0438\u0435"
+					}
+				) {
+					
+					public boolean isCellEditable(int row, int column) {
+				        
+						if(column == 0 || row == 0) return false;
+						
+						return true;
+				    }
+					
+				});
+				
+			}
+		});
+		list.setModel(new AbstractListModel<AttributeTypeProperties>() {
+			
+			public int getSize() {
+				return attrs.size();
+			}
+			public AttributeTypeProperties getElementAt(int index) {
+				return attrs.get(index);
+			}
+		});
+		scrollPane_1.setViewportView(list);
+		
+		splitPane.setAutoscrolls(true);
 		
 		setVisible(true);
 		
