62e237f46c6d9a216ae74a7fb837238330095d47
LuchikiSveta <ToBeVoid@yandex.ru>
2026-02-17 22:14:56 +0300

    add: Add popup menu

--- src/main/GUI/AdminWindow.java
+++ src/main/GUI/AdminWindow.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
 import javax.swing.border.EmptyBorder;
 
 import API.AttributeTypeProperties;
@@ -17,12 +18,16 @@ import API.interfaces.IUserSession;
 import javax.swing.JTabbedPane;
 import javax.swing.JSplitPane;
 import javax.swing.JList;
+import javax.swing.JMenuItem;
 import javax.swing.JTable;
 import javax.swing.AbstractListModel;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.JScrollPane;
 import javax.swing.SpringLayout;
 import javax.swing.JButton;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
@@ -34,12 +39,15 @@ public class AdminWindow extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTable table;
+	
+	AttributeTypeProperties editAttr = null;
+	
 	/**
 	 * Create the frame.
 	 */
 	public AdminWindow(IUserSession session) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 629, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
@@ -92,27 +100,63 @@ public class AdminWindow extends JFrame {
 			
 			public void mouseClicked(MouseEvent e) {
 				
+				if(editAttr != null) {
+					
+					list.setSelectedValue(editAttr, true);
+					list.repaint();
+					return;
+					
+				}
+				
 				AttributeTypeProperties attr = list.getSelectedValue();
 				
-				table.setModel(new DefaultTableModel(
-					new Object[][] {
-						{"Идентификатор", attr.attributeTypeID},
-						{"Наименование", attr.attributeName},
-						{"Тип значения", attr.valueAttributeType},
-					},
-					new String[] {
-						"\u041F\u0430\u0440\u0430\u043C\u0435\u0442\u0440", "\u0417\u043D\u0430\u0447\u0435\u043D\u0438\u0435"
-					}
-				) {
+				if(e.getButton() == 3) {
 					
-					public boolean isCellEditable(int row, int column) {
-				        
-						if(column == 0 || row == 0) return false;
+					JPopupMenu menu = new JPopupMenu();
+					
+					JMenuItem addBtn = new JMenuItem("Добавить");
+					
+					menu.add(addBtn);
+					
+					addBtn.addActionListener(new ActionListener() {
 						
-						return true;
-				    }
+						public void actionPerformed(ActionEvent e) {
+							
+							AttributeTypeProperties attr = new AttributeTypeProperties();
+							
+							attr.attributeTypeID = -1;
+							attr.attributeName = "";
+							attr.valueAttributeType = 1;
+							
+							setTable(attr);
+							
+							btnNewButton.setEnabled(true);
+							btnNewButton_1.setEnabled(true);
+							
+							attrs.add(attr);
+							
+							list.setModel(new AbstractListModel<AttributeTypeProperties>() {
+								
+								public int getSize() {
+									return attrs.size();
+								}
+								public AttributeTypeProperties getElementAt(int index) {
+									return attrs.get(index);
+								}
+							});
+							
+							editAttr = attr;
+							
+						}
+					});
 					
-				});
+					menu.show(e.getComponent(), e.getX(), e.getY());
+					
+				} else {
+					
+					setTable(attr);
+					
+				}
 				
 			}
 		});
@@ -129,7 +173,61 @@ public class AdminWindow extends JFrame {
 		
 		splitPane.setAutoscrolls(true);
 		
+		btnNewButton.addMouseListener(new MouseAdapter() {
+			
+			public void mouseClicked(MouseEvent e) {
+				
+				btnNewButton.setEnabled(false);
+				btnNewButton_1.setEnabled(false);
+				
+				editAttr = null;
+				list.setSelectedIndex(0);
+				setTable(list.getSelectedValue());
+				
+			}
+			
+		});
+		
+		btnNewButton_1.addMouseListener(new MouseAdapter() {
+			
+			public void mouseClicked(MouseEvent e) {
+				
+				if((int)table.getValueAt(0, 1) == -1) {
+					
+					System.out.println(table.getValueAt(1, 1));
+					
+				}
+				
+			}
+			
+		});
+		
 		setVisible(true);
 		
 	}
+	
+	void setTable(AttributeTypeProperties attr) {
+		
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{"Идентификатор", attr.attributeTypeID},
+				{"Наименование", attr.attributeName},
+				{"Тип значения", attr.valueAttributeType},
+			},
+			new String[] {
+				"\u041F\u0430\u0440\u0430\u043C\u0435\u0442\u0440", "\u0417\u043D\u0430\u0447\u0435\u043D\u0438\u0435"
+			}
+		) {
+			
+			public boolean isCellEditable(int row, int column) {
+		        
+				if(column == 0 || row == 0) return false;
+				
+				return true;
+		    }
+			
+		});
+		
+	}
+	
 }
